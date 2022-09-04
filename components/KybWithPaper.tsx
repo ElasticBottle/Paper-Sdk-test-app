430a35179d5b107103e5c0793abd5aafacaa4cbf
ElasticBottle <winstonyeo99@yahoo.com>
2022-09-03 18:13:03 -0700

    Remove unused ref

--- components/KybWithPaper.tsx
+++ components/KybWithPaper.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export const KybWithPaper = () => {
-  const iFrameRef = useRef<HTMLIFrameElement>(null);
   useEffect(() => {
     const messageHandler = async (event: MessageEvent) => {
       const payload = event.data;
@@ -31,7 +30,6 @@ export const KybWithPaper = () => {
     <iframe
       className="h-80 w-full"
       src="http://localhost:3000/sdk/2022-08-12/seller-onboarding/verify-email?platformId=0x04Fa9bD20b5fac18d537ED29997EC400E9538c6A"
-      ref={iFrameRef}
     />
   );
 };
