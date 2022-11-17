4cabc787489c9b3bd525501373dbfb96b566c6a3
ElasticBottle <winstonyeo99@yahoo.com>
2022-11-16 17:46:30 -0800

    Add more functions

--- components/EmbeddedWalletComponent.tsx
+++ components/EmbeddedWalletComponent.tsx
@@ -9,6 +9,7 @@ export const EmbeddedWalletComponent = () => {
     const setUp = async () => {
       const Paper = new PaperClient({
         clientId: "00fdf7b2-1dd8-4784-a2c3-58d16910a1af",
+        chain: "Mumbai",
       });
       setPaperWalletSdk(Paper);
     };
@@ -26,7 +27,7 @@ export const EmbeddedWalletComponent = () => {
   };
   const getAddress = async () => {
     if (PaperWalletSdk) {
-      const signer = await PaperWalletSdk.User.getSigner({});
+      const signer = PaperWalletSdk.User.getSigner();
       const address = await signer.getAddress();
       console.log("address", address);
     }
@@ -40,7 +41,20 @@ export const EmbeddedWalletComponent = () => {
       console.log("signedMessage", signedMessage);
     }
   };
-  const signTransaction = async () => {
+  const signTransactionEth = async () => {
+    if (PaperWalletSdk) {
+      const signer = await PaperWalletSdk.User.getSigner({
+        rpcEndpoint: "mainnet",
+      });
+      const tx = {
+        to: "0x8ba1f109551bD432803012645Ac136ddd64DBA72",
+        value: ethers.utils.parseEther("0.1"),
+      };
+      const signedTransaction = await signer.signTransaction(tx);
+      console.log("signedTransaction", signedTransaction);
+    }
+  };
+  const signTransactionGoerli = async () => {
     if (PaperWalletSdk) {
       const signer = await PaperWalletSdk.User.getSigner({
         rpcEndpoint: "goerli",
@@ -51,6 +65,30 @@ export const EmbeddedWalletComponent = () => {
       };
       const signedTransaction = await signer.signTransaction(tx);
       console.log("signedTransaction", signedTransaction);
+    }
+  };
+  const callContractGasless = async () => {
+    if (PaperWalletSdk) {
+      console.log(
+        "await PaperWalletSdk.User.details.walletAddress",
+        await PaperWalletSdk.User.details.walletAddress
+      );
+      const params = {
+        contractAddress: "0xb2369209b4eb1e76a43fAd914B1d29f6508c8aae",
+        method: {
+          args: [await PaperWalletSdk.User.details.walletAddress, 1, 0],
+          stub: "function claimTo(address _to, uint256 _tokeIt, uint256 _quantity) external" as const,
+        },
+      };
+      console.log("params", params);
+      try {
+        const { transactionHash } = await PaperWalletSdk.User.writeTo.contract(
+          params
+        );
+        console.log("transactionHash", transactionHash);
+      } catch (e) {
+        console.error(`something went wrong sending gasless transaction ${e}`);
+      }
     }
   };
 
@@ -84,10 +122,22 @@ export const EmbeddedWalletComponent = () => {
         SignMessage
       </button>
       <button
-        onClick={signTransaction}
+        onClick={signTransactionEth}
         className="m-2 rounded-xl bg-orange-600 px-4 py-2 hover:bg-orange-700 active:bg-orange-800"
       >
-        SignTransaction
+        SignTransaction Eth
+      </button>
+      <button
+        onClick={signTransactionGoerli}
+        className="m-2 rounded-xl bg-orange-600 px-4 py-2 hover:bg-orange-700 active:bg-orange-800"
+      >
+        SignTransaction Goerli
+      </button>
+      <button
+        onClick={callContractGasless}
+        className="m-2 rounded-xl bg-orange-600 px-4 py-2 hover:bg-orange-700 active:bg-orange-800"
+      >
+        call contract gasless
       </button>
     </div>
   );
